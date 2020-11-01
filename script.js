f62ef2a63881d528eb87e20ed4ff708283e7697b
Mike Belliveau <mbelliveau@gmail.com>
2020-10-31 17:07:04 -0700

    Added modal form functionality.

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ setInterval(function () {
   $("#time").text(now);
 }, 1000);
 
-
+// Date Picker Function
+$(document).ready(function(){
+  var date_input=$('input[name="date"]');
+  var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+  var options={
+    format: 'mm/dd/yyyy',
+    container: container,
+    todayHighlight: true,
+    autoclose: true,
+  };
+  date_input.datepicker(options);
+})
 
